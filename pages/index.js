a8984820732baf837b45920182edaabd46995580
everton <everton.junio132@gmail.com>
2025-01-08 11:14:16 -0300

    mensagem alterada

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home(){
-    return <h1>My darling, i love you like the sun love the flowers in spring 🌺</h1>;
+    return <h1>🌺 My dirling, i love you like the sun love the flowers in spring 🌺</h1>;
 }
 export default Home;
