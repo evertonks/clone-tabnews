736c4a1d6a8ccba6cc76341bfeafdd62e8e985a4
everton <everton.junio132@gmail.com>
2025-01-08 10:56:11 -0300

    adicionado emoji a frase principal

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home(){
-    return <h1>My darling, i love you like the sun love the flowers in spring!</h1>;
+    return <h1>My darling, i love you like the sun love the flowers in spring 🌺</h1>;
 }
 export default Home;
