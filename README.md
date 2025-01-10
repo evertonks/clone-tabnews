b3c90cb9d6d1a42e17e02ea2a93f352a6ce1c063
everton <everton.junio132@gmail.com>
2025-01-10 10:59:11 -0300

    adicionado scripts  e

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para praticar aulas do curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
-function Home(){
-    return <h1>🌺 My dirling, i love you like the sun love the flowers in spring 🌺</h1>;
+function Home() {
+  return (
+    <h1>
+      🌺 My dirling, i love you like the sun love the flowers in spring 🌺
+    </h1>
+  );
 }
 export default Home;
