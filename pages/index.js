a22e93c6449c7d3dfd82c2565cfde461baf3f18b
everton <everton.junio132@gmail.com>
2025-01-15 14:20:31 -0300

    imagem

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,10 @@ function Home() {
       <h1>
         🌺 My dirling, i love you like the sun love the flowers in spring 🌺
       </h1>
-      <img href="../images/familia.jpg"></img>
+      <img
+        src="https://scontent.fcpv12-1.fna.fbcdn.net/v/t39.30808-6/325454738_539664851558413_5816773142796017391_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=86c6b0&_nc_eui2=AeHdwzGi0-nT0-0Ep_Dkk1fX4DlQOTObVZbgOVA5M5tVlq7ELB98g6YPqPN7KMTw0jF7YhTD15PQrNZsIa0CKEql&_nc_ohc=H75kuS1KO0sQ7kNvgFBfraD&_nc_zt=23&_nc_ht=scontent.fcpv12-1.fna&_nc_gid=AwNDMWV97YyVBVZmGoFlQmw&oh=00_AYDbfpgJTQsIUyB0bCvoVICIJ3e8xeFKiDvI7K-Rh3OE5Q&oe=678DDCD7"
+        alt="familia"
+      ></img>
     </>
   );
 }
