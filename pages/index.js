c7b8eaed8a5ff3379a33c5b7a9072fa87dccc2ae
everton <everton.junio132@gmail.com>
2025-01-15 14:16:03 -0300

    imagem

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 function Home() {
   return (
-    <h1>
-      🌺 My dirling, i love you like the sun love the flowers in spring 🌺
-    </h1>
+    <>
+      <h1>
+        🌺 My dirling, i love you like the sun love the flowers in spring 🌺
+      </h1>
+      <img href="../images/familia.jpg"></img>
+    </>
   );
 }
 export default Home;
