4526ada4eb1e1d809c055642d69f3d9fcc2b1780
everton <everton.junio132@gmail.com>
2025-01-27 15:05:22 -0300

    finish  endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,33 @@
 import database from 'infra/database.js';
 async function status(req, res) {
-  const result = await database.query('SELECT 1 + 1 as sum;');
-  console.log(result.rows);
-  res.status(200).json({ mensagem: 'tudo ok' });
+  const databaseVersionResult = await database.query('SHOW server_version;');
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    'SHOW max_connections;',
+  );
+  const databaseMaxConnectionsValue =
+    await databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const databaseOpenedConnectionsResult = await database.query({
+    text: 'SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;',
+    values: [databaseName],
+  });
+
+  const databaseConnectionsValue = databaseOpenedConnectionsResult.rows.length;
+
+  const updatedAt = new Date().toISOString();
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseConnectionsValue,
+      },
+    },
+  });
 }
 export default status;
